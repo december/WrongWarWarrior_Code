3cc1484859845f3cdb7085632c64b2737d33d6fb
luyunfei.louis <luyunfei.louis@bytedance.com>
2018-11-25 10:51:33 +0200

    update source code

---
+++ Assets/Scripts/Princess.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Princess : MonoBehaviour {
+
+    private Animator anim;
+    private int randomTime;
+    private float lastTime;
+    public enum State {Sleep, Shout, Saved};
+    public State state = State.Sleep;
+    bool saved = false;
+    GameObject cam;
+    public GameObject fin,finObj;
+    // Use this for initialization
+    void Start () {
+        randomTime = Random.Range(8, 15);
+        lastTime = Time.fixedTime;
+        anim = GetComponentInChildren<Animator>();
+    }
+
+    IEnumerator Return()
+    {
+        yield return new WaitForSeconds(4);
+        SceneManager.LoadScene(1);
+    }
+
+	// Update is called once per frame
+	void Update () {
+        switch (state)
+        {
+            case State.Sleep:
+                float tempTime = Time.fixedTime;
+                if (tempTime - lastTime >= randomTime)
+                {
+                    lastTime = tempTime;
+                    randomTime = Random.Range(8, 15);
+                    anim.SetTrigger("WantToShout");
+                }
+                break;
+            case State.Shout:
+                break;
+            case State.Saved:
+                break;
+        }
+
+        if (saved)
+        {
+            cam.transform.position = GameObject.Find("Warrior").transform.position + new Vector3(0F, 0.6F, -10F);
+            cam.GetComponent<Camera>().orthographicSize = 2F;
+            finObj.transform.position = GameObject.Find("Warrior").transform.position + new Vector3(0F, 1.6F);
+        }
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Warrior")
+            GetSaved();
+    }
+
+    public void GetSaved(){
+        if (saved) return;
+        cam = GameObject.Find("Main Camera");
+        state = State.Saved;
+        anim.SetTrigger("Saved");
+        saved = true;
+        Warrior wr = GameObject.Find("Warrior").GetComponent<Warrior>();
+        wr.FindPrincess();
+        StartCoroutine(Return());
+        finObj = GameObject.Instantiate(fin, GameObject.Find("Warrior").transform.position + new Vector3(0F, 1.6F), Quaternion.identity);
+    }
+}
---
+++ Assets/Scripts/Reverser.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Reverser : MonoBehaviour {
+
+    public enum State {First, Second, Third };
+    float alpha = 1F, sTime = 0.2F;
+    State state = State.First;
+    Vector3 startPos;
+    public bool leftward = true;
+    void Reverse()
+    {
+        if (gameObject.tag == "Warrior")
+        {
+            GetComponent<Warrior>().reversed = !GetComponent<Warrior>().reversed;
+            if(GetComponent<Warrior>().reversed)
+                GameObject.Instantiate(GetComponent<Warrior>().note2, new Vector3(100, 0), Quaternion.identity);
+            else
+                GameObject.Instantiate(GetComponent<Warrior>().note3, new Vector3(100, 0), Quaternion.identity);
+        }
+        if (gameObject.tag == "Boar")
+            GetComponent<Boar>().reversed = !GetComponent<Boar>().reversed;
+        if (gameObject.tag == "Bird")
+            GetComponent<Bird>().reversed = !GetComponent<Bird>().reversed;
+    }
+
+    // Use this for initialization
+    void Start () {
+        startPos = transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        switch (state)
+        {
+            case State.First:
+                alpha -= 3F * Time.deltaTime;
+                GetComponent<SpriteRenderer>().color = new Color(1F, 1F, 1F, alpha);
+                if (alpha <= 0F)
+                {
+                    state = State.Second;
+                    Reverse();
+                }
+                break;
+            case State.Second:
+                sTime -= Time.deltaTime;
+                if (sTime <= 0F)
+                {
+                    state = State.Third;
+                    gameObject.transform.position = startPos + (leftward ? 0.1F : -0.1F) * Vector3.left;
+                }
+                break;
+            case State.Third:
+                alpha += 3F * Time.deltaTime;
+                GetComponent<SpriteRenderer>().color = new Color(1F, 1F, 1F, alpha);
+                if (alpha >= 1F)
+                    Destroy(this);
+                break;
+
+        }
+    }
+}
